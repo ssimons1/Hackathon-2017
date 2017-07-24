a078efc62ddd29ae617fe861bf61c538be69ac38
Yerusalmi <ryerusalmi@gmail.com>
2017-07-24 17:04:48 +0300

    Thank you

--- javascript/sitter.js
+++ javascript/sitter.js
@@ -1,3 +1,9 @@
 $('#submitSitter').on('click', function(){
 	$(document).scrollTop( 0 );
+	$('body').css('overflow', 'hidden')
+	$('.pendingRequest').fadeIn();
+
+setTimeout(function(){ window.location.href = './owner.html' }, 3000);
+
+
 })
